edf821edd888669ea97ec7bcbd145eeebdadc946
Yann Rabiller <yann.rabiller@fortrabbit.com>
2022-01-28 11:01:45 +0100

    Add vscode

--- install.sh
+++ install.sh
@@ -49,6 +49,7 @@ sudo pamac install --no-confirm \
 yay ruby-neovim # need to find a way to automate this (with no confirm)
 yay sublime-text-4
 yay code-minimap
+yay visual-studio-code-bin # uses the proprietary version instead of the openvsx one
 sudo pip install neovim
 sudo pip3 install neovim
 sudo groupadd docker
