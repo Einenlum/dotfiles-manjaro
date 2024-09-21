0fd311efb3207b717536b27a6c2232cca9633ccf
Einenlum <contact@einenlum.com>
2024-09-21 15:41:20 +0200

    Remove black plugin

--- nvim/lua/einenlum/plugins/black.lua
+++
@@ -1,7 +0,0 @@
-local api = vim.api
-
-vim.g.black_virtualenv = "/home/einenlum/.local/pipx/venvs/black"
-api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.py",
-    command = "execute ':Black'"
-})
