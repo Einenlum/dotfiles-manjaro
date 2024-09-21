12ab8ffe919ee1dc039f17b276d346948b8243f3
Einenlum <contact@einenlum.com>
2024-09-21 16:03:52 +0200

    Add first and end keys to fzf keymaps

--- nvim/lua/einenlum/plugins/fzf.lua
+++ nvim/lua/einenlum/plugins/fzf.lua
@@ -1,27 +1,33 @@
-require('fzf-lua').setup{
-  colorschemes = {
-    ignore_patterns = {
-        "^blue$",
-        "^darkblue$",
-        "^default$",
-        "^delek$",
-        "^desert$",
-        "^elflord$",
-        "^evening$",
-        "^habamax$",
-        "^industry$",
-        "^koehler$",
-        "^lunaperche$",
-        "^morning$",
-        "^murphy$",
-        "^pablo$",
-        "^peachpuff$",
-        "^quiet$",
-        "^ron$",
-        "^shine$",
-        "^slate$",
-        "^torte$",
-        "^zellner$",
+require('fzf-lua').setup {
+    keymap = {
+        fzf = {
+            ["home"] = "first",
+            ["end"]  = "last",
+        },
+    },
+    colorschemes = {
+        ignore_patterns = {
+            "^blue$",
+            "^darkblue$",
+            "^default$",
+            "^delek$",
+            "^desert$",
+            "^elflord$",
+            "^evening$",
+            "^habamax$",
+            "^industry$",
+            "^koehler$",
+            "^lunaperche$",
+            "^morning$",
+            "^murphy$",
+            "^pablo$",
+            "^peachpuff$",
+            "^quiet$",
+            "^ron$",
+            "^shine$",
+            "^slate$",
+            "^torte$",
+            "^zellner$",
+        }
     }
-  }
 }
