70a2869bbc0e9d25e737cfec748aed227cf1440f
Einenlum <contact@einenlum.com>
2024-09-23 13:17:50 +0200

    Use a command to use phpactor under the hood when I need to rename a class

--- nvim/lua/einenlum/plugins/phpactor.lua
+++ nvim/lua/einenlum/plugins/phpactor.lua
@@ -1,11 +1,24 @@
--- I use fzf-lua instead of fzf.vim, which is not compatible with
--- phpactor right now
--- vim.g.phpactorInputListStrategy = 'phpactor#input#list#fzf'
--- vim.g.phpactorQuickfixStrategy = "phpactor#quickfix#fzf"
-require'lspconfig'.phpactor.setup{
-    on_attach = on_attach,
-    init_options = {
-        ["language_server_phpstan.enabled"] = false,
-        ["language_server_psalm.enabled"] = false,
-    }
-}
+function PhpactorClassMove(new_file)
+  -- Get full path of the current file
+  local current_file = vim.fn.expand('%:p')
+
+  if not new_file or new_file == '' then
+    new_file = vim.fn.input('New path for class: ', current_file)
+    if not new_file or new_file == '' then
+      print('Opération annulée.')
+      return
+    end
+  end
+
+  local cmd = 'phpactor class:move ' .. vim.fn.shellescape(current_file) .. ' ' .. vim.fn.shellescape(new_file)
+
+  vim.cmd('!' .. cmd)
+
+  -- Update the buffer with the new file
+  vim.cmd('edit ' .. new_file)
+end
+
+-- register a PHPRename function into vim so that I can do :PHPRename
+vim.api.nvim_create_user_command('PHPRename', function(opts)
+  PhpactorClassMove(opts.args)
+end, { nargs = '?' })
