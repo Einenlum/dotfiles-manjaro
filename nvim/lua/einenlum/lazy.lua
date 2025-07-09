4acd0e4870e6c20e677f076e74492e61f8b3484c
Einenlum <contact@einenlum.com>
2025-07-09 15:52:35 +0200

    add typescript tools and fix formatting

--- nvim/lua/einenlum/lazy.lua
+++ nvim/lua/einenlum/lazy.lua
@@ -1,265 +1,269 @@
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-    if vim.v.shell_error ~= 0 then
-        error('Error cloning lazy.nvim:\n' .. out)
-    end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end
 -- end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-
 -- Initialize LazyVim
 require("lazy").setup({
 
-    -- Start
-    { 'mhinz/vim-startify' }, -- Nice start menu in vim
+	-- Start
+	{ "mhinz/vim-startify" }, -- Nice start menu in vim
 
-    -- Icons
-    { 'nvim-tree/nvim-web-devicons' }, -- icons for status line
-    { 'ryanoasis/vim-devicons' },    -- icons in nerdtree and fzf
+	-- Icons
+	{ "nvim-tree/nvim-web-devicons" }, -- icons for status line
+	{ "ryanoasis/vim-devicons" }, -- icons in nerdtree and fzf
 
-    -- Colors
-    { 'gko/vim-coloresque' }, -- show colors after hexadecimal codes
+	-- Colors
+	{ "gko/vim-coloresque" }, -- show colors after hexadecimal codes
 
-    -- Colorschemes
-    { 'rktjmp/lush.nvim' }, -- Tool used by some colorschemes
-    { 'kyazdani42/blue-moon' },
-    { 'AlessandroYorba/Alduin' },
-    { 'FrenzyExists/aquarium-vim' },
-    { 'JoosepAlviste/palenightfall.nvim' },
-    { 'comfysage/evergarden' },
-    { 'embark-theme/vim' },
-    { 'folke/tokyonight.nvim' },
-    { 'glepnir/oceanic-material' },
-    { 'nvimdev/zephyr-nvim' },
-    { 'rakr/vim-two-firewatch' },
-    { 'ramojus/mellifluous.nvim' },
-    { 'ray-x/aurora' },
-    { 'rose-pine/neovim' },
-    { 'sainnhe/everforest' },
-    { 'sainnhe/gruvbox-material' },
-    { 'savq/melange-nvim' },
-    { 'shaunsingh/nord.nvim' },
-    { 'AlexvZyl/nordic.nvim',                branch = 'main' },
-    { 'Yazeed1s/oh-lucy.nvim' },
-    { 'aktersnurra/no-clown-fiesta.nvim' },
-    { 'catppuccin/nvim',                     as = 'catppuccin' },
-    { 'dgox16/oldworld.nvim' },
-    { 'mcchrish/zenbones.nvim' },
-    { 'nyoom-engineering/oxocarbon.nvim' },
-    { 'rebelot/kanagawa.nvim' },
+	-- Colorschemes
+	{ "rktjmp/lush.nvim" }, -- Tool used by some colorschemes
+	{ "kyazdani42/blue-moon" },
+	{ "AlessandroYorba/Alduin" },
+	{ "FrenzyExists/aquarium-vim" },
+	{ "JoosepAlviste/palenightfall.nvim" },
+	{ "comfysage/evergarden" },
+	{ "embark-theme/vim" },
+	{ "folke/tokyonight.nvim" },
+	{ "glepnir/oceanic-material" },
+	{ "nvimdev/zephyr-nvim" },
+	{ "rakr/vim-two-firewatch" },
+	{ "ramojus/mellifluous.nvim" },
+	{ "ray-x/aurora" },
+	{ "rose-pine/neovim" },
+	{ "sainnhe/everforest" },
+	{ "sainnhe/gruvbox-material" },
+	{ "savq/melange-nvim" },
+	{ "shaunsingh/nord.nvim" },
+	{ "AlexvZyl/nordic.nvim", branch = "main" },
+	{ "Yazeed1s/oh-lucy.nvim" },
+	{ "aktersnurra/no-clown-fiesta.nvim" },
+	{ "catppuccin/nvim", as = "catppuccin" },
+	{ "dgox16/oldworld.nvim" },
+	{ "mcchrish/zenbones.nvim" },
+	{ "nyoom-engineering/oxocarbon.nvim" },
+	{ "rebelot/kanagawa.nvim" },
 
-    -- Language
-    { 'kikito/inspect.lua' }, -- Help debug in lua
-    { 'jparise/vim-graphql' }, -- Syntax highlighting in graphql
+	-- Language
+	{ "kikito/inspect.lua" }, -- Help debug in lua
+	{ "jparise/vim-graphql" }, -- Syntax highlighting in graphql
 
-    -- Autocompletion
-    { 'nvim-lua/plenary.nvim' },
-    { 'j-hui/fidget.nvim',                   tag = 'legacy' },
+	-- Autocompletion
+	{ "nvim-lua/plenary.nvim" },
+	{ "j-hui/fidget.nvim", tag = "legacy" },
 
-    -- AI
-    {
-      "yetone/avante.nvim",
-      event = "VeryLazy",
-      version = false, -- Never set this value to "*"! Never!
-      opts = {
-        -- add any opts here
-        -- for example
-        -- provider = "openai",
-        -- openai = {
-        --   endpoint = "https://api.openai.com/v1",
-        --   model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-        --   timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-        --   temperature = 0,
-        --   max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-        --   --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-            providers = {
-              openrouter = {
-                __inherited_from = 'openai',
-                endpoint = 'https://openrouter.ai/api/v1',
-                api_key_name = 'OPENROUTER_API_KEY',
-                model = 'deepseek/deepseek-chat-v3-0324:free',
-              },
-            },
-           provider = "openrouter",
-      },
-      -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-      build = "make",
-      -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-      dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "stevearc/dressing.nvim",
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        --- The below dependencies are optional,
-        "echasnovski/mini.pick", -- for file_selector provider mini.pick
-        "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-        "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-        "ibhagwan/fzf-lua", -- for file_selector provider fzf
-        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-        {
-          "zbirenbaum/copilot.lua",
-          event = "InsertEnter",
-          opts = {
-            suggestion = {
-              enabled = true,
-              auto_trigger = true,
-              keymap = {
-                accept = "<C-s>",
-                next = "<M-]>",
-                prev = "<M-[>",
-                dismiss = "<C-]>",
-              },
-            },
-            panel = { enabled = false },
-          },
-        },
-        {
-          -- support for image pasting
-          "HakonHarnes/img-clip.nvim",
-          event = "VeryLazy",
-          opts = {
-            -- recommended settings
-            default = {
-              embed_image_as_base64 = false,
-              prompt_for_file_name = false,
-              drag_and_drop = {
-                insert_mode = true,
-              },
-              -- required for Windows users
-              use_absolute_path = true,
-            },
-          },
-        },
-        {
-          -- Make sure to set this up properly if you have lazy=true
-          'MeanderingProgrammer/render-markdown.nvim',
-          opts = {
-            file_types = { "markdown", "Avante" },
-          },
-          ft = { "markdown", "Avante" },
-        },
-      },
-    },
+	-- AI
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		version = false, -- Never set this value to "*"! Never!
+		opts = {
+			-- add any opts here
+			-- for example
+			-- provider = "openai",
+			-- openai = {
+			--   endpoint = "https://api.openai.com/v1",
+			--   model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+			--   timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+			--   temperature = 0,
+			--   max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+			--   --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+			providers = {
+				openrouter = {
+					__inherited_from = "openai",
+					endpoint = "https://openrouter.ai/api/v1",
+					api_key_name = "OPENROUTER_API_KEY",
+					model = "deepseek/deepseek-chat-v3-0324:free",
+				},
+			},
+			provider = "openrouter",
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"echasnovski/mini.pick", -- for file_selector provider mini.pick
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua", -- for file_selector provider fzf
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			{
+				"zbirenbaum/copilot.lua",
+				event = "InsertEnter",
+				opts = {
+					suggestion = {
+						enabled = true,
+						auto_trigger = true,
+						keymap = {
+							accept = "<C-s>",
+							next = "<M-]>",
+							prev = "<M-[>",
+							dismiss = "<C-]>",
+						},
+					},
+					panel = { enabled = false },
+				},
+			},
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
 
-    -- LSP Support
-    { 'neovim/nvim-lspconfig' },
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
-    { 'jay-babu/mason-null-ls.nvim' },   -- plug mason with null ls
-    { 'jose-elias-alvarez/null-ls.nvim' }, -- Add formatting support
-    {
-      "OlegGulevskyy/better-ts-errors.nvim",
-      dependencies = { "MunifTanjim/nui.nvim" },
-      opts = {
-        keymaps = {
-          toggle = '<leader>md', -- default '<leader>dd'
-          go_to_definition = '<leader>mt' -- default '<leader>dx'
-        }
-      }
-    },
+	-- LSP Support
+	{ "neovim/nvim-lspconfig" },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "jay-babu/mason-null-ls.nvim" }, -- plug mason with null ls
+	{ "nvimtools/none-ls.nvim" }, -- Add formatting support
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {},
+	},
+	{
+		"OlegGulevskyy/better-ts-errors.nvim",
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {
+			keymaps = {
+				toggle = "<leader>md", -- default '<leader>dd'
+				go_to_definition = "<leader>mt", -- default '<leader>dx'
+			},
+		},
+	},
 
-    -- Autocompletion Plugins
-    { 'hrsh7th/nvim-cmp' },
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-nvim-lsp-signature-help' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/cmp-nvim-lua' },
-    { 'hrsh7th/cmp-path' },
-    { 'hrsh7th/cmp-vsnip' },
-    { 'hrsh7th/vim-vsnip' },
-    { 'hrsh7th/vim-vsnip-integ' },
-    { 'saadparwaiz1/cmp_luasnip' },
+	-- Autocompletion Plugins
+	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-nvim-lsp-signature-help" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-nvim-lua" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-vsnip" },
+	{ "hrsh7th/vim-vsnip" },
+	{ "hrsh7th/vim-vsnip-integ" },
+	{ "saadparwaiz1/cmp_luasnip" },
 
-    -- Rust
-    { 'simrat39/rust-tools.nvim' },
+	-- Rust
+	{ "simrat39/rust-tools.nvim" },
 
-    -- Debugging
-    { 'mfussenegger/nvim-dap' },
-    { 'nvim-neotest/nvim-nio' },
-    { 'rcarriga/nvim-dap-ui' },
-    { 'jayp0521/mason-nvim-dap.nvim' },
+	-- Debugging
+	{ "mfussenegger/nvim-dap" },
+	{ "nvim-neotest/nvim-nio" },
+	{ "rcarriga/nvim-dap-ui" },
+	{ "jayp0521/mason-nvim-dap.nvim" },
 
-    -- Databases
-    { 'tpope/vim-dadbod' },
-    { 'kristijanhusak/vim-dadbod-ui' },
-    { 'kristijanhusak/vim-dadbod-completion' },
+	-- Databases
+	{ "tpope/vim-dadbod" },
+	{ "kristijanhusak/vim-dadbod-ui" },
+	{ "kristijanhusak/vim-dadbod-completion" },
 
-    -- Snippets
-    { 'L3MON4D3/LuaSnip' },
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
+	-- Snippets
+	{ "L3MON4D3/LuaSnip" },
+	{ "VonHeikemen/lsp-zero.nvim", branch = "v4.x" },
 
-    -- Diagnostics
-    { 'folke/trouble.nvim' },
+	-- Diagnostics
+	{ "folke/trouble.nvim" },
 
-    {
-        'stevearc/conform.nvim',
-        opts = {},
-    },
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
 
-    -- Snippets
-    { 'SirVer/ultisnips' }, -- Snippets
+	-- Snippets
+	{ "SirVer/ultisnips" }, -- Snippets
 
-    -- Linting
-    { 'vim-syntastic/syntastic' },
+	-- Linting
+	{ "vim-syntastic/syntastic" },
 
-    -- Git
-    { 'tpope/vim-fugitive' },          -- add git integration
+	-- Git
+	{ "tpope/vim-fugitive" }, -- add git integration
 
-    { -- Adds git related signs to the gutter, as well as utilities for managing changes
-        'lewis6991/gitsigns.nvim',
-        opts = {
-            signs = {
-                add = { text = '+' },
-                change = { text = '~' },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                changedelete = { text = '~' },
-        },
-        },
-    },
+	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
 
-    -- Fuzzy finders
-    { 'ibhagwan/fzf-lua' }, -- Add fzf menu
+	-- Fuzzy finders
+	{ "ibhagwan/fzf-lua" }, -- Add fzf menu
 
-    -- Vim menu and behavior
-    { 'sjbach/lusty' },            -- Lusty explorer, allows ,lr and ,lf to navigate between files
-    { 'bling/vim-bufferline' },    -- Display a line of buffers
-    { 'nvim-lualine/lualine.nvim' }, -- status line
-    { 'meuter/lualine-so-fancy.nvim' },
-    { 'scrooloose/nerdtree' },
-    { 'moll/vim-bbye' },               -- Close a buffer without messing up the layout
-    { 'editorconfig/editorconfig-vim' }, -- Use editorconfig file
-    -- Allow editing as root
-    { 'lambdalisue/suda.vim' },
+	-- Vim menu and behavior
+	{ "sjbach/lusty" }, -- Lusty explorer, allows ,lr and ,lf to navigate between files
+	{ "bling/vim-bufferline" }, -- Display a line of buffers
+	{ "nvim-lualine/lualine.nvim" }, -- status line
+	{ "meuter/lualine-so-fancy.nvim" },
+	{ "scrooloose/nerdtree" },
+	{ "moll/vim-bbye" }, -- Close a buffer without messing up the layout
+	{ "editorconfig/editorconfig-vim" }, -- Use editorconfig file
+	-- Allow editing as root
+	{ "lambdalisue/suda.vim" },
 
-    -- Code navigation
-    { 'Einenlum/yaml-revealer' },                             -- Display and search in yaml files
-    { 'tmhedberg/SimpylFold' },                               -- Help folding in Python
-    { 'stephpy/vim-yaml' },                                   -- Avoid slowliness with yaml files (is it still useful?)
-    { 'luochen1990/rainbow' },                                -- Colors brackets according to levels
-    { 'mechatroner/rainbow_csv' },                            -- Syntax highlighting + sql queries on CSV
-    { 'phaazon/hop.nvim' },                                   -- Easy motions 1
-    { 'ggandor/lightspeed.nvim' },                            -- Easy motions 2
-    { 'ggandor/leap.nvim' },
-    { 'RRethy/vim-illuminate' },                              -- highlight keywords and values when staying on them
-    { 'nvim-treesitter/nvim-treesitter',     build = ':TSUpdate' }, -- TSUpdate updates all the parsers
-    { 'nvim-treesitter/playground',          build = ':TSInstall query' },
+	-- Code navigation
+	{ "Einenlum/yaml-revealer" }, -- Display and search in yaml files
+	{ "tmhedberg/SimpylFold" }, -- Help folding in Python
+	{ "stephpy/vim-yaml" }, -- Avoid slowliness with yaml files (is it still useful?)
+	{ "luochen1990/rainbow" }, -- Colors brackets according to levels
+	{ "mechatroner/rainbow_csv" }, -- Syntax highlighting + sql queries on CSV
+	{ "phaazon/hop.nvim" }, -- Easy motions 1
+	{ "ggandor/lightspeed.nvim" }, -- Easy motions 2
+	{ "ggandor/leap.nvim" },
+	{ "RRethy/vim-illuminate" }, -- highlight keywords and values when staying on them
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }, -- TSUpdate updates all the parsers
+	{ "nvim-treesitter/playground", build = ":TSInstall query" },
 
-    -- Code manipulation
-    { 'FooSoft/vim-argwrap' }, -- multi-line single line function
-    { 'godlygeek/tabular' },  -- Allows to align things with :Tabularize
-    { 'tpope/vim-commentary' }, -- Easy comment with gc
-    { 'tpope/vim-abolish' },  -- Search and substitute words respecting case
-    { 'jiangmiao/auto-pairs' }, -- Complete brackets and quotes
+	-- Code manipulation
+	{ "FooSoft/vim-argwrap" }, -- multi-line single line function
+	{ "godlygeek/tabular" }, -- Allows to align things with :Tabularize
+	{ "tpope/vim-commentary" }, -- Easy comment with gc
+	{ "tpope/vim-abolish" }, -- Search and substitute words respecting case
+	{ "jiangmiao/auto-pairs" }, -- Complete brackets and quotes
 
-    -- File manipulation
-    { 'vim-scripts/Rename' }, -- Rename the file in buffer
+	-- File manipulation
+	{ "vim-scripts/Rename" }, -- Rename the file in buffer
 
-    -- Games
-    { 'ThePrimeagen/vim-be-good' }, -- Practice vim skills
+	-- Games
+	{ "ThePrimeagen/vim-be-good" }, -- Practice vim skills
 })
--- nvim/lua/einenlum/plugins/lsp.lua
+++ nvim/lua/einenlum/plugins/lsp.lua
@@ -1,107 +1,133 @@
-local lsp = require('lsp-zero')
-
+local lsp = require("lsp-zero")
 
 local lsp_attach = function(client, bufnr)
-      local opts = {buffer = bufnr}
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
---   lsp.default_keymaps({buffer = bufnr})
+	local opts = { buffer = bufnr }
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	--   lsp.default_keymaps({buffer = bufnr})
 
-    vim.keymap.set('n', '<leader>gt', function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set('n', '<leader>cws', function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set('n', '<leader>cc', function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set('v', '<leader>cb', '<leader>cc5<CR>', opts)
-    -- vim.keymap.set('n', '<leader>cca', function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set('n', '<leader>rf', function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set('n', '<leader>crn', function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set('n', '<leader>ci', function() vim.lsp.buf.implementation() end, opts)
-    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
-    vim.keymap.set('n', '<leader>e', function() vim.diagnostic.open_float() end, opts)
+	vim.keymap.set("n", "<leader>gt", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>cws", function()
+		vim.lsp.buf.workspace_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>cc", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	vim.keymap.set("v", "<leader>cb", "<leader>cc5<CR>", opts)
+	-- vim.keymap.set('n', '<leader>cca', function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set("n", "<leader>rf", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>crn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("n", "<leader>ci", function()
+		vim.lsp.buf.implementation()
+	end, opts)
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
+	vim.keymap.set("n", "<leader>e", function()
+		vim.diagnostic.open_float()
+	end, opts)
 end
 
-lsp.extend_lspconfig({
-  capabilities = require('cmp_nvim_lsp').default_capabilities(),
-  lsp_attach = lsp_attach,
-  float_border = 'rounded',
-  sign_text = {
-    error = '✘',
-    warn = '▲',
-    hint = '⚑',
-    info = ''
-  },
-})
-
+-- lsp.extend_lspconfig({
+--   capabilities = require('cmp_nvim_lsp').default_capabilities(),
+--   lsp_attach = lsp_attach,
+--   float_border = 'rounded',
+--   sign_text = {
+--     error = '✘',
+--     warn = '▲',
+--     hint = '⚑',
+--     info = ''
+--   },
+-- })
 
 -- Custom handler for 'textDocument/definition'
 -- The problem is that when I jump on a PHP class definition,
 -- I get two results: one for the class and one for the constructor
 local function goto_definition(_, result, ctx, _)
-  if result == nil or vim.tbl_isempty(result) then
-    vim.notify('[LSP] Definition not found', vim.log.levels.INFO)
-    return nil
-  end
+	if result == nil or vim.tbl_isempty(result) then
+		vim.notify("[LSP] Definition not found", vim.log.levels.INFO)
+		return nil
+	end
 
-  -- If multiple results, check if they are in the same file
-  if vim.tbl_islist(result) then
-    local first_uri = result[1].uri or result[1].targetUri
-    local same_file = true
+	-- If multiple results, check if they are in the same file
+	if vim.tbl_islist(result) then
+		local first_uri = result[1].uri or result[1].targetUri
+		local same_file = true
 
-    for _, location in ipairs(result) do
-      local uri = location.uri or location.targetUri
-      if uri ~= first_uri then
-        same_file = false
-        break
-      end
-    end
+		for _, location in ipairs(result) do
+			local uri = location.uri or location.targetUri
+			if uri ~= first_uri then
+				same_file = false
+				break
+			end
+		end
 
-    if same_file then
-      -- Jump to the first location if all are in the same file
-      vim.lsp.util.jump_to_location(result[1], 'utf-8')
-    else
-      -- Show all locations if they are in different files
-      vim.lsp.handlers['textDocument/definition'](_, result, ctx, _)
-    end
-  else
-    -- Single result, jump directly
-    vim.lsp.util.jump_to_location(result, 'utf-8')
-  end
+		if same_file then
+			-- Jump to the first location if all are in the same file
+			vim.lsp.util.jump_to_location(result[1], "utf-8")
+		else
+			-- Show all locations if they are in different files
+			vim.lsp.handlers["textDocument/definition"](_, result, ctx, _)
+		end
+	else
+		-- Single result, jump directly
+		vim.lsp.util.jump_to_location(result, "utf-8")
+	end
 end
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-    -- By default tailwincss is enabled on php files. We explicitely
-    -- set the filetypes it should be enabled on.
-    ["tailwindcss"] = function()
-      require('lspconfig').tailwindcss.setup({
-        filetypes = { "html", "blade", "css", "scss", "less", "javascriptreact", "typescriptreact", "vue", "svelte" },
-      })
-    end,
-    -- Avoid having the lua language server complain about
-    -- the 'vim' global variable being not defined
-    ["lua_ls"] = function()
-      require('lspconfig').lua_ls.setup({
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-          },
-        },
-      })
-    end,
-    ["intelephense"] = function()
-      require('lspconfig').intelephense.setup({
-        handlers = {
-          ['textDocument/definition'] = goto_definition,
-        },
-      })
-    end,
-  },
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	handlers = {
+		function(server_name)
+			require("lspconfig")[server_name].setup({})
+		end,
+		-- By default tailwincss is enabled on php files. We explicitely
+		-- set the filetypes it should be enabled on.
+		["tailwindcss"] = function()
+			require("lspconfig").tailwindcss.setup({
+				filetypes = {
+					"html",
+					"blade",
+					"css",
+					"scss",
+					"less",
+					"javascriptreact",
+					"typescriptreact",
+					"vue",
+					"svelte",
+				},
+			})
+		end,
+		-- Avoid having the lua language server complain about
+		-- the 'vim' global variable being not defined
+		["lua_ls"] = function()
+			require("lspconfig").lua_ls.setup({
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
+		end,
+		["intelephense"] = function()
+			require("lspconfig").intelephense.setup({
+				handlers = {
+					["textDocument/definition"] = goto_definition,
+				},
+			})
+		end,
+	},
 })
 
 -- lspconfig = require('lspconfig')
